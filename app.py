f589af686d60067c153a2abc1a774972b79c1d58
juanflask <enolancharles@gmail.com>
2022-03-17 19:50:32 +0000

    editar formulario -incompleto- (clase 8)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, redirect
-from forms import formulario
+from flask import Flask, render_template, request, url_for, redirect, abort
+from forms import formulario, form_crea_articulos
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
@@ -35,6 +35,25 @@ def contacto():
 	else:
 		return render_template("contacto.html", form=form)
 
+@app.route("/crear_articulos", methods=["GET", "POST"])
+def crea_articulos():
+	form = form_crea_articulos(request.form)
+	if request.method == "POST":
+		titulo= form.titulo.data
+		articulo = form.articulo.data
+
+		conn = get_db_connection()
+		conn.execute('INSERT INTO articulos (titulo, articulo) VALUES (?,?)',
+			(titulo, articulo))
+		conn.commit()
+		conn.close()
+
+		return redirect(url_for('index'))
+		# A modificar después 
+	
+	else:
+		return render_template("crea_articulos.html", form=form)
+
 
 @app.route("/mensajes")
 def mensajes():
@@ -46,9 +65,34 @@ def mensajes():
 @app.route("/articulos")
 def articulos():
 	conn = get_db_connection()
-	articulos = conn.execute('SELECT * FROM articulos').fetchall()
+	articulos = conn.execute('SELECT * FROM articulos ORDER BY fecha DESC').fetchall()
 	conn.close()
 	return render_template('articulos.html', articulos=articulos)
+
+@app.route("/<int:id>/edit", methods=["GET", "POST"])
+def editar_post():
+	conn = get_db_connection()
+	articulo = conn.execute('SELECT * FROM articulos WHERE id=?', (id)).fetchone()
+	conn.close()
+	if articulo is None:
+		abort(404)
+
+	form = form_crea_articulos(request.form)
+
+	if request.method == "POST":
+		titulo= form.titulo.data
+		articulo = form.articulo.data
+
+		conn = get_db_connection()
+		conn.execute('UPDATE articulos SET titulo = ?, articulo= ? WHERE id=?', (titulo, articulo, id))
+
+		conn.commit()
+		conn.close()
+
+		return redirect(url_for('articulos'))
+
+	return render_template('edit_post.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,14 @@
-from wtforms import Form, StringField, EmailField, TextAreaField, validators
+from wtforms import Form, StringField, EmailField, TextAreaField, SubmitField, DateTimeField, validators
 
 class formulario(Form):
     name = StringField("name", [validators.Length(min=3, max=25)])
     email = EmailField("email", [validators.Length(min=3, max=25)])
     text = TextAreaField("Mensaje", [validators.Length(min=3, max=200)])
 
+class form_crea_articulos(Form):
+    titulo = StringField("titulo", [validators.Length(min=3, max=25)])
+    autor = StringField("autor", [validators.Length(min=3, max=25)])
+    articulo = TextAreaField("articulo")
+    button = SubmitField("Enviar artículo")
 
 
